cb47f936c90b116b8432024d9922636bb4393a54
Darwin <balldarwin638@gmail.com>
2025-05-30 19:55:58 -0600

    otros cambios render backend

--- Backend/run.py
+++ Backend/run.py
@@ -21,7 +21,8 @@ app = create_app()
 #app.register_blueprint(personas_bp, url_prefix='/api')  # Prefijo opcional '/api'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
     
     
